a4a1ad698b7a9b8160625c8eb74f06cfd9c51877
beratberkayg <berat.1675@gmail.com>
2023-11-07 13:25:17 +0300

    last

--- app/redux/DataSlice.ts
+++ app/redux/DataSlice.ts
@@ -20,9 +20,12 @@ export const DataSlice = createSlice({
     kullanıcıEkle: (state, action: PayloadAction<DataState>) => {
       state.data.push(action.payload);
     },
+    kullanıcıSil: (state, action: PayloadAction<string>) => {
+      return state.data.filter((dt) => dt.isim !== action.payload);
+    },
   },
 });
 
-export const { kullanıcıEkle } = DataSlice.actions;
+export const { kullanıcıEkle, kullanıcıSil } = DataSlice.actions;
 
 export default DataSlice.reducer;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,18 +1,31 @@
 "use client";
 
-import { useAppSelector } from "../redux/hooks";
+import { kullanıcıSil } from "../redux/DataSlice";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
 
 const Users = () => {
+    const dispatch = useAppDispatch();
   const { data } = useAppSelector((state) => state.data);
   console.log(data);
+
+  
+  
   return (
     <div>
       <p>kullanıcılar</p>
       {data.map((dt, i) => (
+        <div>
         <div key={i}>
-          {dt.isim} + {dt.soyisim}
+          hoş Geldin -> {dt.isim} {dt.soyisim}
         </div>
+        
+        </div>
+        
+            
       ))}
+
+
+      
     </div>
   );
 };
